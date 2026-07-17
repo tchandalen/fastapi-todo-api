d2c7ece5d113d22cabf958627f5e9b807634c711
tchandalen <tcdl.teangchandalen@gmail.com>
2026-07-17 10:54:41 +0700

    hz hz

--- src/config/mongo.py
+++ src/config/mongo.py
@@ -5,7 +5,7 @@ def collections(name):
     username = os.getenv('MONGO_USERNAME')
     password = os.getenv('MONGO_PASSWORD')
     host = os.getenv('MONGO_HOST')
-    port = os.getenv('MONGO_PORT')
+    port = int(os.getenv('MONGO_PORT'))
     url = f'mongodb://{username}:{password}@{host}:{port}/'
     client = MongoClient(url)
     return client['todo'][name]
